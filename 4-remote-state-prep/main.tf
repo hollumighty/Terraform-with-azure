48dc07d81f69e5ef5d64a912c95daf301be89b25
olumide <hollumighty95@gmail.com>
2023-09-13 23:26:50 +0100

    create azure remote storage with SAS token

---
+++ 4-remote-state-prep/main.tf
@@ -0,0 +1,136 @@
+#############################################################################
+# TERRAFORM CONFIG
+#############################################################################
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+#############################################################################
+# VARIABLES
+#############################################################################
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "naming_prefix" {
+  type    = string
+  default = "itma"
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "azurerm" {
+  features {}
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+#CREATE THE BLOB STORAGE
+
+resource "random_integer" "sa_num" {
+  min = 10000
+  max = 99999
+}
+
+
+resource "azurerm_resource_group" "setup" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "${lower(var.naming_prefix)}${random_integer.sa_num.result}"
+  resource_group_name      = azurerm_resource_group.setup.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
+
+#A CONTAINER TO HOLD THE STATE FILE
+
+resource "azurerm_storage_container" "ct" {
+  name                 = "terraform-state"
+  storage_account_name = azurerm_storage_account.sa.name
+
+}
+
+#A SAS TOKEN WITH NECCESARY PERMISSION
+
+data "azurerm_storage_account_sas" "state" {
+  connection_string = azurerm_storage_account.sa.primary_connection_string
+  https_only        = true
+
+  resource_types {
+    service   = true
+    container = true
+    object    = true
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  start  = timestamp()
+  expiry = timeadd(timestamp(), "17520h")
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = true
+    list    = true
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+}
+
+#############################################################################
+# LOCAL FILE
+#############################################################################
+
+#STORE THE OUTPUT IN A FILE
+
+resource "local_file" "post-config" {
+  depends_on = [azurerm_storage_container.ct]
+
+  filename = "${path.module}/backend-config.txt"
+  content  = <<EOF
+storage_account_name = "${azurerm_storage_account.sa.name}"
+container_name = "terraform-state"
+key = "terraform.tfstate"
+sas_token = "${data.azurerm_storage_account_sas.state.sas}"
+
+  EOF
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.setup.name
+}
